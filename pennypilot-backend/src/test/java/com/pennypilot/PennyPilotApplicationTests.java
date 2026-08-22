5a799e52ce66604c93fc66d1d31140c01884144a
sojalrajurkar-blip <sojalrajurkar@gmail.com>
2026-08-22 23:10:07 +0530

    Configure H2 in-memory test profile for clean unit test execution

--- pennypilot-backend/src/test/java/com/pennypilot/PennyPilotApplicationTests.java
+++ pennypilot-backend/src/test/java/com/pennypilot/PennyPilotApplicationTests.java
@@ -2,8 +2,10 @@ package com.pennypilot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PennyPilotApplicationTests {
 
 	@Test
@@ -11,3 +13,4 @@ class PennyPilotApplicationTests {
 	}
 
 }
+
